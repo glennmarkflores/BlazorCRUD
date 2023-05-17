e8b60ef72898c24cc79e3dbdbbe3af3730545bc8
GLENN MARK FLORES <floresglennmark546@gmail.com>
2023-05-17 16:27:21 +0800

    feat: Add Hero

--- BlazorCRUD/Client/Services/SuperHeroService/SuperHeroService.cs
+++ BlazorCRUD/Client/Services/SuperHeroService/SuperHeroService.cs
@@ -20,9 +20,12 @@ namespace BlazorCRUD.Client.Services.SuperHeroService
             throw new NotImplementedException();
         }
 
-        public Task<SuperHero> GetSingleHero(int id)
+        public async Task<SuperHero> GetSingleHero(int id)
         {
-            throw new NotImplementedException();
+            var result = await _http.GetFromJsonAsync<SuperHero>($"api/superhero/{id}");
+            if (result != null)
+                return result;
+            throw new Exception("Hero Not Found");
         }
 
         public async Task GetSuperHeroes()

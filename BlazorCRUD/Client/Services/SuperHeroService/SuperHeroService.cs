9ea949ef775178759c65e13543971d2d1f03d629
GLENN MARK FLORES <floresglennmark546@gmail.com>
2023-05-17 16:07:54 +0800

    Add project files.

---
+++ BlazorCRUD/Client/Services/SuperHeroService/SuperHeroService.cs
@@ -0,0 +1,37 @@
+﻿using BlazorCRUD.Shared;
+using System.Net.Http.Json;
+
+namespace BlazorCRUD.Client.Services.SuperHeroService
+{
+    public class SuperHeroService : ISuperHeroService
+    {
+        private readonly HttpClient _http;
+
+        public List<SuperHero> Heroes { get; set; } = new List<SuperHero>();
+        public List<Comic> Comics { get; set; } = new List<Comic>();
+
+        public SuperHeroService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public Task GetComics()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SuperHero> GetSingleHero(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task GetSuperHeroes()
+        {
+            var result = await _http.GetFromJsonAsync<List<SuperHero>>("api/superhero");
+            if(result != null)
+            {
+                Heroes = result;
+            }
+        }
+    }
+}
